cabb4ebe6d038448a4dcc69baf5a402112d8e1b6
olgahusada3 <ohusada@icloud.com>
2022-01-06 20:22:06 +0700

    Camera & Result Page + Fonts

---
+++ Skripsi AMIN/Skripsi AMIN/Feature/Result Page/imageModel.swift
@@ -0,0 +1,14 @@
+//
+//  imageModel.swift
+//  Skripsi AMIN
+//
+//  Created by Olga Husada on 04/01/22.
+//
+
+import Foundation
+import UIKit
+class ImageModel{
+    static let shared =  ImageModel()
+    
+    var image: UIImage!
+}
