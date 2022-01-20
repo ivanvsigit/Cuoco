08c34e75d05f3c346d910bba66c7bf12fcf7dd7e
olgahusada3 <ohusada@icloud.com>
2022-01-20 16:38:26 +0700

    - Color Navbar Result + Launch screem

---
+++ Skripsi AMIN/Skripsi AMIN/Feature/Launch Screen/LaunchViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LaunchViewController.swift
+//  Skripsi AMIN
+//
+//  Created by Olga Husada on 20/01/22.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+
+    @IBOutlet weak var image: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        image.animationImages = (0...2).map ({
+            value in return UIImage(named: "animated-3-\(value)") ?? UIImage()
+        })
+        image.animationRepeatCount = -1
+        image.animationDuration = 1
+        image.startAnimating()
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
